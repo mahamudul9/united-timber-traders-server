289d014ce888cd6b1c9b924bfe2d171ef42c0b6f
mahamudul9 <mahmudulrana9@gmail.com>
2021-04-21 12:15:33 +0600

    7th

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ywjyr.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
+  console.log(err);
   const serviceCollection = client.db("unitedTimberDB").collection("unitedTimber");
   const bookingCollection = client.db("unitedTimberDB").collection("bookings");
   const adminCollection = client.db("unitedTimberDB").collection("admins");
